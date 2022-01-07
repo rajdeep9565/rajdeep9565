8f988bcfcd66e61777676f9a62fee70adacb4b34
RAJDEEP KUMAR SINGH <87421854+rajdeep9565@users.noreply.github.com>
2022-01-07 20:26:19 +0530

    Update README.js

--- README.js
+++ README.js
@@ -1,10 +1,40 @@
-- 👋 Hi, I’m @rajdeep9565
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
+<img align="right" src="https://i.pinimg.com/originals/6e/a8/c6/6ea8c68dfa924bc2e6a9abe3e473087a.gif" alt="Coder GIF" width="420" height="330">
 
-<!---
-rajdeep9565/rajdeep9565 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+
+
+### Hi there, I am Rajdeep Kumar Singh 👋
+[![Linkedin Badge](https://img.shields.io/badge/-Abhishek-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/abhishek-singh-4b0999b9//)](https://www.linkedin.com/in/abhishek-singh-4b0999b9/)
+[![Gmail Badge](https://img.shields.io/badge/-Abhishek-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:singh80020@gmail.com)](mailto:singh80020@gmail.com) 
+
+- 🎯 Portfolio website: [Portfolio](https://github.com/rajdeep9565)
+- ⚡ Fun fact: The first rule of programming- if it works, don’t touch it.🤓
+
+💻 *Things I love*
+- Javascript <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30"> 
+- React.js <img src="https://3ulsmb4eg8vz37c0vz2si64j-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/react-native-UX-design.gif" width="30"> 
+- MongoDB <img src="https://www.pistalix.in/wp-content/uploads/2018/11/mongodb.gif" width="30"> 
+- Nodejs <img src="https://raw.githubusercontent.com/yoavain/create-windowless-app/main/resources/docs/logo.gif" width="30"> 
+- Tailwind <img src="https://raw.githubusercontent.com/app-generator/static/master/tailwind-css/tailwind-css-components-intro.gif" width="30"> 
+- Express <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--RI_lzIWT--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_66%2Cw_880/https://cdn-images-1.medium.com/max/800/0%2APTKhCN2p9S8EDZ4r.gif" width="30"> 
+
+    <a href="https://github.com/Abhisingh755/github-readme-stats" title="Go to Source">
+      <img align="right" width=420 height="auto" src="https://github-readme-stats.vercel.app/api?username=Abhisingh755&show_icons=true&theme=dark&border_color=61dafb&hide_border=true&include_all_commits=true" />
+    </a>
+	
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![Tailwind](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)
+![ReactJS](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
+![NodeJS](https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)
+![MongoDB](https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white)
+![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)
+![JWT](https://img.shields.io/badge/JWT-000000?style=for-the-badge&logo=JSON%20web%20tokens&logoColor=white)
+![Heroku](https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white)
+![Express](https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white)
+![AWS](https://img.shields.io/badge/Amazon_AWS-FF9900?style=for-the-badge&logo=amazonaws&logoColor=white)
+
+
+<div align="center">
+    <h3 align="center">Show some&nbsp;<img src="https://c.tenor.com/sfX_wg0N8IsAAAAM/love-beating.gif" width="30"> &nbsp; by starring &nbsp;  <img src="https://bestanimations.com/media/stars/139102408gold-star-animation9.gif" width="30">&nbsp; some of the repositories!</h3>
+</div>
